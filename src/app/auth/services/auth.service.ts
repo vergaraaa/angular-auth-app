5209640c90893cb8f72125db8bbc77b0dec44463
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-20 08:37:51 +0200

    ✨ auth service and interfaces

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
