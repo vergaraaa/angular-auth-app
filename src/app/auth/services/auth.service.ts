50f316d3293a9c51f1dfd3e16abfb864a08c3377
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-20 09:36:18 +0200

    ✨ refactor auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -33,13 +33,7 @@ export class AuthService {
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-
-        localStorage.setItem('token', token);
-      }),
-      map(() => true),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError((error: HttpErrorResponse) => {
         return throwError(() => error.error.message);
       })
@@ -55,19 +49,21 @@ export class AuthService {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
-      map(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-
-        localStorage.setItem('token', token);
-
-        return true;
-      }),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError(() => {
         this._authStatus.set(AuthStatus.notAuthenticated);
 
         return of(false);
       })
     );
+  }
+
+  private setAuthentication(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+
+    localStorage.setItem('token', token);
+
+    return true;
   }
 }
